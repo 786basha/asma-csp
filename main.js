93fadb4a18d5f18020299cc7cb79355b8ddcd849
786basha <bashamasthan31@gmail.com>
2023-07-12 00:52:24 +0530

    wdwdwd

--- main.js
+++ main.js
@@ -24,3 +24,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// ==================
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
