389562630c22ffcfc4b6d0d0564210f918f87f83
786basha <bashamasthan31@gmail.com>
2023-07-12 23:44:25 +0530

    wdwdwd

--- main.js
+++ main.js
@@ -39,3 +39,20 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// =====================================
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+  return false;
+  }
+  }
